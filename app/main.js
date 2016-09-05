2382f5d995fd1f5632fbeccdfa01d8368de5f57c
Yota Odaka <YotaOdaka@weigaoyangtais-MacBook-Pro.local>
2016-09-05 15:57:27 +0900

    Created KerningTrainingWindow class, which AppManager class has as a member, and updated AppManager class

--- app/main.js
+++ app/main.js
@@ -1,16 +1,53 @@
 // YourTypes main.js
 // Coded by Yota Odaka
+
 'use strict';
 
 require('babel-register');
-const { app, BrowserWindow, crashReporter } = require('electron');
+require('babel-core');
+const { app, Menu, MenuItem,
+        BrowserWindow, crashReporter } = require('electron');
 
+const { KerningTrainingWindow } = require('./kerning_training_window');
+
+class AppManager {
+  constructor() {
+    this.menu = undefined;
+    this.kerningTrainingWindow =
+      new KerningTrainingWindow(`file://${ __dirname }/training.html`);
+  }
+  addApplicationMenu() {
+    this.menu = Menu.getApplicationMenu();
+    if (this.menu == false) {
+      console.log('Application menu is not ready');
+      return;
+    }
+    const additional = new MenuItem({
+      label: 'Applications',
+      submenu: [
+        {
+          label: 'Kerning Training',
+          accelerator: 'CmdOrCtrl+T',
+          click(item, focusedWindow) {
+            this.runKerningTrainingWindow();
+          }
+        }
+      ]
+    });
+    this.menu.insert(1, additional);
+    Menu.setApplicationMenu(this.menu);
+    console.log('Applications menu has been updated');
+  }
+  runKerningTrainingWindow() {
+    this.kerningTrainingWindow.run();
+  }
+
+}
+let appManager = new AppManager();
 
 if (process.env.NODE_ENV === 'develop') {
   crashReporter.start();
 }
-
-let mainWindow = undefined;
 
 app.on('window-all-closed', () => {
   if (process.platform != 'darwin') {
@@ -18,10 +55,6 @@ app.on('window-all-closed', () => {
   }
 });
 app.on('ready', () => {
-  const options = { width: 800, height: 600 };
-  mainWindow = new BrowserWindow(options);
-  mainWindow.loadURL(`file://${ __dirname }/index.html`);
-  mainWindow.on('closed', () => {
-    mainWindow = undefined;
-  });
+  appManager.addApplicationMenu();
+  appManager.runKerningTrainingWindow();
 });
