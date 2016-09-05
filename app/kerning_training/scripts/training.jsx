4bcec41bc3a0b7754df97b8736b36b79808facdd
Yota Odaka <YotaOdaka@weigaoyangtais-MacBook-Pro.local>
2016-09-05 20:46:56 +0900

    Made each characters draggable

--- app/kerning_training/scripts/training.jsx
+++ app/kerning_training/scripts/training.jsx
@@ -9,17 +9,46 @@ import TrainingSampleTextView from './view/training_sample_text_view.jsx';
 
 class Training {
   constructor() {
-    // window.addEventListener('load', this.setTrainingText);
     this.text =
       'ある日の事でございます。御釈迦様おしゃかさまは極楽の蓮池はすいけのふちを、独りでぶらぶら御歩きになっていらっしゃいました。池の中に咲いている蓮はすの花は、みんな玉のようにまっ白で、そのまん中にある金色きんいろの蕊ずいからは、何とも云えない好よい匂においが、絶間たえまなくあたりへ溢あふれて居ります。極楽は丁度朝なのでございましょう。';
-
-    this.setTrainingText(this.text);
-    // window.onload = this.setTrainingText;
+    this.setTrainingText(this.text, this.enableCharsToBeDragged);
   }
-  setTrainingText(testText) {
+  enableCharsToBeDragged() {
+    const container =
+      document.getElementsByClassName('kerning-training-field-chars')[0];
+    let chars = container.childNodes;
+    chars.forEach((element, index, array) => {
+      if (index == 0) { return; }
+      element.dragging = false;
+      // Make drag events to each span elements
+      element.addEventListener('mousedown', () => {
+        element.dragging = true;
+        element.pivot = element.clientX;
+      });
+      element.addEventListener('mouseup', () => {
+        element.dragging = false;
+        element.pivot = undefined;
+      });
+      element.addEventListener('mouseout', () => {
+        element.dragging = false;
+        element.pivot = undefined;
+      });
+      element.addEventListener('mousemove',(event) => {
+        if (element.dragging) {
+          const previous = array[index-1];
+          let letterSpace = parseInt(previous.style.letterSpacing, 10);
+            letterSpace += (event.clientX - element.pivot);
+          previous.style.letterSpacing = letterSpace + 'px';
+          element.pivot = event.clientX;
+        }
+      });
+    });
+  }
+  setTrainingText(testText, callback) {
     ReactDOM.render(
       <TrainingSampleTextView text={testText} />,
-      document.getElementsByClassName('kerning-training-field')[0]
+      document.getElementsByClassName('kerning-training-field')[0],
+      callback
     );
   }
 }
--- app/kerning_training/scripts/view/training_sample_text_view.jsx
+++ app/kerning_training/scripts/view/training_sample_text_view.jsx
@@ -13,18 +13,18 @@ export default class TrainingSampleTextView extends React.Component{
     }
   }
   render() {
+    const className = 'kerning-training-field-chars';
+    const style = {
+      letterSpacing: '0px'
+    };
     let chars = [];
     for (let i = 0; i < this.props.text.length; i++) {
-      chars.push(<span key={ i }>{ this.props.text[i] }</span>);
+      chars.push(<span key={ i } style={ style }>{ this.props.text[i] }</span>);
     }
-    console.log(chars);
     return (
-      <div>
+      <div className={ className }>
       { chars }
       </div>
     );
-  }
-  prepareTestWords(wordsLength) {
-    this.state = 'ある日の事でございます。御釈迦様おしゃかさまは極楽の蓮池はすいけのふちを、独りでぶらぶら御歩きになっていらっしゃいました。池の中に咲いている蓮はすの花は、みんな玉のようにまっ白で、そのまん中にある金色きんいろの蕊ずいからは、何とも云えない好よい匂においが、絶間たえまなくあたりへ溢あふれて居ります。極楽は丁度朝なのでございましょう。';
   }
 }
