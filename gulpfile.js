9dcf421a6ab633f0bde5c29e68dd4faa8ffabc93
Yota Odaka <YotaOdaka@weigaoyangtais-MacBook-Pro.local>
2016-09-05 19:26:35 +0900

    Prepared CSS file and environment

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,32 @@ const autoprefixer = require('gulp-autoprefixer');
 
 const dist = './app';
 
-gulp.task('sass', () => {
+gulp.task('sass', ['sass:log', 'sass:impl']);
+
+gulp.task('sass:log', () => {
   console.log('Transpile SCSS to CSS');
-  gulp.src('./app/sass/**/*.scss')
+});
+
+gulp.task('sass:impl', ['sass:impl:top', 'sass:impl:kerning_training']);
+
+gulp.task('sass:impl:top', () => {
+  gulp.src('./app/sass/top/**/*.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(gulp.dest(`${ dist }/css` ));
+    .pipe(gulp.dest(`${ dist }/top/css`));
 });
 
-gulp.task('sass:watch', () => {
+gulp.task('sass:impl:kerning_training', () => {
+  gulp.src('./app/sass/kerning_training/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(`${ dist }/kerning_training/css`));
+});
+
+gulp.task('watch:sass', () => {
   gulp.watch(['./app/sass/**/*.scss', './app/sass/**/_*.scss'], ['sass']);
 });
 
-gulp.task('watch', ['sass:watch']);
+gulp.task('watch', ['watch:sass']);
