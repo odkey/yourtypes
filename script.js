8db0112fe198f655db526fffd764c1a936d61d67
odkey <amglowez@gmail.com>
2016-12-26 16:48:48 +0900

    impl of  a facility of add some value into json data user input; next to do is to refer side space data of each letters

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ $(document).ready(() => {
   button.on('click', (e) => {
     let inputText = $('#input-text');
     let inputJson = $.parseJSON($('#input-text').val());
-    console.log(e);
+    addSideSpaceDataTo(inputJson);
   });
 });
 
 function addSideSpaceDataTo(inputJson) {
-  
+  let jsonObject = inputJson;
+  console.log(jsonObject);
+  jsonObject.values.forEach((e, i) => {
+    e.first_char.densities.left_space = 300;
+    e.first_char.densities.right_space = 200;
+    e.second_char.densities.left_space = 100;
+    e.second_char.densities.right_space = 100;
+  });
+  setTimeout(() => {
+    let output = $('#output-text');
+    let jsonString = JSON.stringify(jsonObject);
+    console.log(jsonString);
+    output.val(jsonString);
+  }, 1000);
 }
